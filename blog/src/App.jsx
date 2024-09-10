d2ea24023e4e36f4afdfaf95fa6ba5faabf52635
sungmin <95615086+Clariatinet@users.noreply.github.com>
2024-09-10 21:45:46 +0900

    9.10 class

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 function App() {
   let [title, setTitle] = useState([
@@ -8,73 +8,109 @@ function App() {
     '강남 우동 맛집',
     '파이썬 독학',
   ]);
-  let [good, setGood] = useState(0);
+  let [good, setGood] = useState([0, 0, 0]);
   let [modal, setModal] = useState(false);
+  let [index, setIndex] = useState(0);
+  let [input, setInput] = useState('');
   return (
     <div className='App'>
       <div className='black-nav'>
         <h4>ReactBlog</h4>
       </div>
+      {title.map(function (a, i) {
+        return (
+          <div className='list' key={i}>
+            <h4
+              onClick={() => {
+                setModal(true);
+                if (modal === true) {
+                  setModal(false);
+                }
+                setIndex(i);
+              }}
+            >
+              {title[i]}
+              <span
+                onClick={(e) => {
+                  e.stopPropagation();
+                  let copy = [...good];
+                  copy[i] = copy[i] + 1;
+                  setGood(copy);
+                }}
+              >
+                👍
+              </span>{' '}
+              {good[i]}
+            </h4>
+            <p>2월 17일 발행</p>
+            <button
+              onClick={() => {
+                let copy = [...title];
+                copy.splice(i, 1);
+                setTitle(copy);
+              }}
+            >
+              삭제
+            </button>
+          </div>
+        );
+      })}
+      <input
+        type='text'
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
+      />
       <button
         onClick={() => {
-          title.sort();
-          setTitle([...title]);
+          let copy = [...title];
+          copy.unshift(input);
+          setTitle(copy);
         }}
       >
-        가나다순 정렬
+        입력
       </button>
-      <div className='list'>
-        <h4>
-          {title[0]}
-          <button
-            onClick={() => {
-              title[0] = '여자 코트 추천';
-              setTitle([...title]);
-            }}
-          >
-            글수정
-          </button>
-          <span
-            onClick={() => {
-              setGood(good + 1);
-            }}
-          >
-            👍
-          </span>{' '}
-          {good}
-        </h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className='list'>
-        <h4>{title[1]}</h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className='list'>
-        <h4
-          onClick={() => {
-            setModal(true);
-            if (modal === true) {
-              setModal(false);
-            }
-          }}
-        >
-          {title[2]}
-        </h4>
-        <p>2월 17일 발행</p>
-      </div>
-      {modal === true ? <Modal /> : null}
+      {modal === true ? (
+        <Modal title={title} setTitle={setTitle} index={index} />
+      ) : null}
+      <Modal2 />
     </div>
   );
 }
 
-function Modal() {
+function Modal(props) {
   return (
     <div className='modal'>
-      <h4>제목</h4>
+      <h4>{props.title[props.index]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
+      <button>글 수정</button>
     </div>
   );
+}
+
+class Modal2 extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'Kim',
+      age: 20,
+    };
+  }
+  render() {
+    return (
+      <div>
+        안녕 {this.state.age}
+        <button
+          onClick={() => {
+            this.setState({ age: 21 });
+          }}
+        >
+          버튼
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
